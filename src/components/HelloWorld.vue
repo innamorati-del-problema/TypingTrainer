a092e9f027331e14f68f75b2787ee84b8b19bdab
Riccardo Tittarelli <tittarelli.1883338@studenti.uniroma1.it>
2022-04-13 17:46:34 +0200

    Co-authored-by: lucreziarusso21 <lucreziarusso21@users.noreply.github.com>

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://vuejs.org/">Vue 3</a>. What's next?
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+  background: linear-gradient(178.19deg, #72C53F 1.53%, #5A9D31 132.54%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-shadow: 0px 4px 16px rgba(84, 169, 130, 0.6);
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
