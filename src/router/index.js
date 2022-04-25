31e94dd729f7cc869dfa481bc52487623a93065c
Riccardo Tittarelli <tittarelli.1883338@studenti.uniroma1.it>
2022-04-25 19:45:00 +0200

    Consodilazione della tastiera  in unico componente

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ const router = createRouter({
         { path: "/textpractice", component: () => import("../components/TextPractice.vue")},
         { path: "/letterpractice", component: () => import("../components/SingleLetterPractice.vue")},
         { path: "/profile", component: () => import("../components/Profile.vue")},
+        { path: "/test", component: () => import("../components/Test.vue")},
     ]
 });
 
